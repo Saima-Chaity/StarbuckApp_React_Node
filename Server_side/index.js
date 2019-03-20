8e57062f623098c0f0692e02377e9f127507cb4b
Saima Chaity <saima.s.chaity@gmail.com>
2019-03-20 11:14:18 -0700

    Changed the mongodb url

--- Server_side/index.js
+++ Server_side/index.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     app = express(),
     bodyParser = require('body-parser');
-
+    var http= require('http');
     
 var coffeeRoutes = require("./routes/coffee");
 var teaRoutes = require("./routes/tea");
@@ -11,10 +11,14 @@ var bakeryRoutes = require("./routes/bakery");
 
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/coffee');
+
 
 mongoose.Promise = Promise;
-
+mongoose.connect('mongodb://recipereact:recipereact25@ds025263.mlab.com:25263/recipereact').then(() => {
+console.log("Connected to Database");
+}).catch((err) => {
+    console.log("Not Connected to Database ERROR! ", err);
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -34,8 +38,6 @@ app.use('/api/bakery', bakeryRoutes);
 
 const PORT = 8081;
 
-app.listen(PORT, function(){
+app.listen(PORT, function(req, res){
     console.log(`APP IS RUNNING ON PORT ${PORT}`);
-})
-    
-    
+});
