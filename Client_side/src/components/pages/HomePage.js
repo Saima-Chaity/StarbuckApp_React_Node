4fffa237131d2505a51f241cefd7caf4231608a5
Saima-Chaity <saima.s.chaity@gmail.com>
2019-03-24 15:23:28 -0700

    Made the homepage responsive

--- Client_side/src/components/pages/HomePage.js
+++ Client_side/src/components/pages/HomePage.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
 import HomePageNav from "../nav/HomePageNav"
 import {Grid} from 'semantic-ui-react'
+import '../../stylesheets/landingPage.css';
 import 'font-awesome/css/font-awesome.min.css';
 
 class  HomePage extends Component {
   
 render(){
-
-
     return(
       <div>
         <HomePageNav/> 
@@ -22,11 +21,11 @@ render(){
 
               <Grid.Row>
               <Grid.Column>
-                <div style={{position: 'relative',width: '100%', height: '500px', background:"grey", color:"white"}}>
+                <div className="coffeeFinder">
                     <div style={{float:"right", margin:"40px"}}>
-                        <img src="https://globalassets.starbucks.com/assets/24e0ef1330ce468ba949ae92eef35222.png" width="600px" height="400px" alt = "img" />
+                        <img className="landingPageImage" src="https://globalassets.starbucks.com/assets/24e0ef1330ce468ba949ae92eef35222.png" alt = "img" />
                     </div>
-                    <div style={{position: 'absolute',top: "30px", left:"30px", width: '50%', height: 'auto'}}>
+                    <div className="columnDescription">
                         <h2>COFFEE FINDER</h2>
                         <p>Our coffee masters have distilled their years of tasting knowledge down to three simple questions to help you find a coffee you’re sure to love.</p>
                     </div>
@@ -36,9 +35,9 @@ render(){
 
              <Grid.Row>
               <Grid.Column>
-                <div className="ui fluid image">
-                    <img src="http://www.fleetwalkshoppingtorquay.co.uk/new2016/wp-content/uploads/2016/10/bg-how_to_brew-1024x341.jpg" alt = "img" width="100%" height="450px"/>
-                    <div style={{position: 'absolute', top: "30px", left:"30px", width: '50%', height: 'auto', color:"white"}}>
+                <div>
+                    <img className="landingPagelastImage" src="http://www.fleetwalkshoppingtorquay.co.uk/new2016/wp-content/uploads/2016/10/bg-how_to_brew-1024x341.jpg" alt = "img"/>
+                    <div className="columnDescriptionForBrewing">
                         <h2>HOW TO BREW</h2>
                         <p>It’s surprising how different brewing methods can enhance particular characteristics in your coffee. Let us help you unlock the full potential of your coffee—for the perfect cup every time.</p>
                     </div>
@@ -48,23 +47,15 @@ render(){
 
              <Grid.Row>
               <Grid.Column>
-                <div style={{position: 'relative',width: '100%', height: '150px', textAlign: "center", top: "30px"}}>
+                <div className="socialMediaIcon">
                     <h2>Contact</h2>
-                    <div style={{fontSize: "30px", padding:"0px 30px"}}>
-                        <a href ={"http://www.facebook.com/starbuckscanada"}><i className="fa fa-facebook-square"></i></a>
-                        <a href ={"http://instagram.com/starbuckscanada#"}><i className="fa fa-instagram"></i></a>
-                        <a href ={"http://www.twitter.com/starbuckscanada"}><i className="fa fa-twitter"></i></a>
-                    </div> 
+                      <a href ={"http://www.facebook.com/starbuckscanada"}><i className="fa fa-facebook-square"></i></a>
+                      <a href ={"http://instagram.com/starbuckscanada#"}><i className="fa fa-instagram"></i></a>
+                      <a href ={"http://www.twitter.com/starbuckscanada"}><i className="fa fa-twitter"></i></a>
                  </div> 
               </Grid.Column>
             </Grid.Row>
         </Grid>
-
-
-        
-
-        
-       
            
       </div>
         
